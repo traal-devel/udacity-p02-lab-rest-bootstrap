5d9422a36dcfcc65159dba387ffd304b23d3bc52
jkowalski@tvfactory.ch <jkowalski@tvfactory.ch>
2019-11-17 12:33:44 +0100

    chore: sprint kw46, first revision

---
+++ src/main/java/ch/traal/demo/UdacityP02Ex03RestBootstrapApplication.java
@@ -0,0 +1,13 @@
+package ch.traal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdacityP02Ex03RestBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdacityP02Ex03RestBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/traal/demo/UdacityP02Ex03RestBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.traal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdacityP02Ex03RestBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
