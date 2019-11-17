f14bdf9913652e9cb4acd3f91385a55d2528e717
devel@traal.ch <devel@traal.ch>
2019-11-17 13:33:55 +0100

    chore: imporve code syntax.

--- src/main/java/ch/traal/demo/repository/LocationRepository.java
+++ src/main/java/ch/traal/demo/repository/LocationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import ch.traal.demo.entity.Location;
 
-public interface LocationRepository extends CrudRepository<Location, Long>{
+public interface LocationRepository extends CrudRepository<Location, Long> {
 
 }
